3ce4e0e78c56b2d0b3ed5c97c852669c1d5ea880
Maciej Sawicki <maciej_sawicki@epam.com>
2016-07-20 11:12:20 +0200

    app init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3333/',
+    'webpack/hot/dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
